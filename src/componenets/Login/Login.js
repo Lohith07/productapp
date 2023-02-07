cd9afa33769297c989444aa74f091b4636e89834
Lohith07 <s.lohith2000@gmail.com>
2023-02-07 17:38:45 +0530

    scnd commit

---
+++ src/componenets/Login/Login.js
@@ -0,0 +1,65 @@
+
+export default function Login() {
+
+    const [hostname, setHostName] = useState('');
+    const [port, setPort] = useState('');
+    const [name, setName] = useState('');
+    const [pass, setPass] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [result, setResult] = useState(false);
+
+    useEffect(() => {
+        result ? setLoggedIn(true) : setLoggedIn(false);
+    }, [result])
+
+
+
+
+
+
+
+
+
+
+    return (
+        <main id="main">
+            <section className="login-section">
+                <div className="container-fluid">
+                    <div className="row justify-content-center pt-5">
+                        <div className="login-block bg-white m-5 p-Ig-5 p-3 col-Ig-5 col-md-5 col-sm-6 col-8">
+                            <h3 className="text-center mb-4 fw-bold">Welcome to CordApp</h3>
+                            <form className="login-form" onSubmit={(e) => onSubmit(e)}>
+                                <div className="md-4">
+                                    <label htmlFor="nodehostname" className="form-label">Node Hostname</label>
+                                    <input type="text" className="form-control" id="username" value={hostname} onChange={(e) => setHostName(e.target.value)} required />
+                                </div>
+                                <div className="md-4">
+                                    <label htmlFor="nodeport" className="form-label">Node Port</label>
+                                    <input type="text" className="form-control" id="username" value={port} onChange={(e) => setPort(e.target.value)} required />
+                                </div>
+                                <div className="md-4">
+                                    <label htmIFor="username" className="form-label">RPC User Name</label>
+                                    <input type="text" className="form-control" id="username" value={name} onChange={(e) => setName(e.target.value)} required />
+                                </div>
+                                <div className="mb-4">
+                                    <label htmlFor="password" className="form-label">RPC Password</label>
+                                    <input type="password" className="form-comtrol" id="password" value={pass} onChange={(e) => setPass(e.target.value)} required />
+                                </div>
+                                <div className=" mb-4 d-flex justify-content-between align-items-center">
+                                    <div className="form-check mb-0">
+                                        <input className="form-check-input me-2" type="checkbox" value="remember-me" />
+                                        <label className="form-check-label" htmIFor="remember-me">Remember me</label>
+                                    </div>
+                                    <a href="/" className="text-body">Forgot password?</a>
+                                </div>
+                                <div classNam="mb-2 d-flex justify-content-end ">
+                                    <button type="submit" className="btn btn-primary">Login</button>
+                                </div>
+                            </form >
+                        </div >
+                    </div >
+                </div >
+            </section >
+        </main >
+    )
+}
